4aa38e5c37c37f39df1e88290b89ccee33f15e49
Kirill Nyuhtilin <kirill.nyuhtilin@gmail.com>
2021-11-09 21:25:32 +0300

    Update userAPI ( me(), users() ) , Create dialogAPI | createDialog, openDialog, getAllDialog

--- api/api.js
+++ api/api.js
@@ -30,5 +30,55 @@ export const userAPI = {
     },
     refresh(){
         return instance.get(`refresh/`)
-    }
+    },
+
+
+    me(){
+        return instance.get(`me/`, {
+            headers: {
+              'x-access-token': 'token-value'
+            }
+        })
+    },
+    users(){
+        return instance.get(`users/`, {
+            headers: {
+              'x-access-token': 'token-value'
+            }
+        })
+    },
+}
+
+export const dialogAPI = {
+    createDialog(partner, message){
+        return instance.post(`create-dialog/`,
+        {
+            partner: partner, 
+            message: message
+        },
+        {
+            headers: {
+                "x-access-token": "token-value",
+            },
+        }
+    )},
+
+    openDialig(id_dialog){
+        return instance.get(`dialog/${id_dialog}`,
+        {
+            headers: {
+                "x-access-token": "token-value",
+            },
+        }
+    )},
+
+    getAllDialogs(){
+        return instance.get(`dialogs`,
+        {
+            headers: {
+                "x-access-token": "token-value",
+            },
+        }
+    )},
+
 }
